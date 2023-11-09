700eeac64086f5d4029191a72557930edf4db125
dan-peveto <daniel_peveto@outlook.com>
2023-11-09 15:46:26 -0800

    strikeShouldGetTwoRollBonus

--- src/BowlingGameTest.java
+++ src/BowlingGameTest.java
@@ -1,4 +1,6 @@
 import static org.junit.Assert.assertEquals;
+
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class BowlingGameTest {
@@ -13,6 +15,26 @@ public class BowlingGameTest {
         Game game = new Game();
         rollMany(game, 20, 1);
         assertEquals(20, game.score());
+    }
+    @Test 
+    public void spareShouldGetNextRollBonus() {
+        Game game = new Game();
+        game.roll(5);
+        game.roll(5);
+        game.roll(3);
+        rollMany(game, 17, 0);
+
+        assertEquals(16, game.score());
+    }
+    @Test
+    public void strikeShouldGetTwoRollBonus() {
+        Game game = new Game();
+        game.roll(10); // strike
+        game.roll(2);
+        game.roll(7);
+        rollMany(game, 16, 0);
+
+        assertEquals(28, game.score());
     }
     private static void rollMany(Game game, int rolls, int pins) {
         for (int roll = 0; roll < rolls; roll++)  {
