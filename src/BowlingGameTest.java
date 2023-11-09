e8ff0e1889d6651e020f50db488ae06416f2a846
dan-peveto <daniel_peveto@outlook.com>
2023-11-09 14:41:24 -0800

    allGuttersGameScoreShouldBeZero complete

---
+++ src/BowlingGameTest.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class BowlingGameTest {
+    @Test
+    public void allGuttersGameScoreShouldBeZero() {
+        Game game = new Game();
+        for (int i=0; i<20; i++) {
+            game.roll(0);
+        }
+        assertEquals(0, game.score());
+    }
+    
+}
