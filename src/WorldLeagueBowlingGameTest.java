3da8a9f5d3e3e399b76f9deb4be0ab721d03e315
dan-peveto <daniel_peveto@outlook.com>
2023-11-16 15:20:23 -0800

    completed allGuttersGameScoreShouldBeZero

--- src/WorldLeagueBowlingGameTest.java
+++ src/WorldLeagueBowlingGameTest.java
@@ -5,4 +5,20 @@ import Leagues.WorldLeagueScorer;
 
 public class WorldLeagueBowlingGameTest {
     
+    // All Gutters Game
+    @Test
+    public void allGuttersGameScoreShouldBeZero() {
+        Game game = new Game(new WorldLeagueScorer());
+        rollMany(game, 20, 0);
+    }
+    // allOnesGameShouldScoreTwenty
+    // spareShoulGetFirstRollBonus such that roles 5, 5, 3 = 18
+    // strikeShouldGetThirtyNoBonus, such that roles 10, 2, 7 = 39
+    // perfectGameShouldGetPerfectScore, such that 10 roles of 10 = 300
+
+    private static void rollMany(Game game, int rolls, int pins) {
+        for (int roll = 0; roll < rolls; roll++) {
+            game.roll(pins);
+        }
+    }
 }
