3871f0524e5584498b2289b82163a4792093e9b6
dan-peveto <daniel_peveto@outlook.com>
2023-12-01 10:20:42 -0800

    added Players and ScoreGame

--- src/App.java
+++ src/App.java
@@ -1,30 +1,34 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-import Leagues.USLeagueScorer;
+import Leagues.*;
 
 
 public class App {
     public static void main(String[] args) throws Exception {
         
-    Scanner scanner = new Scanner(System.in); // initiate scanner
-    
-    System.out.print("How maner players: "); // How many Players
-    int numOfPlayers = scanner.nextInt();
-    scanner.nextLine();
-    System.out.println("Number of players: ");
-    int playerNum = 1; // initialize order for the players
-    ArrayList<String> playerList = new ArrayList<String>();//create array of players;
-    while (playerNum <= numOfPlayers) {  // Name each player 
-        System.out.print("Name Player " + playerNum +":");
-        String playerName = scanner.nextLine();
-        playerList.add(playerName);
-        playerNum++;
-    }
-    for(int i=0; i<playerList.size(); i++) {
-        System.out.println(playerList.get(i));
-    }    
-    System.out.println(playerList.size());
+        
+        System.out.print("How maner players: "); // How many Players
+        Scanner scanner = new Scanner(System.in);
+        int numOfPlayers = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Number of players: ");
+        int playerNum = 1; // initialize order for the players
+        ArrayList<String> playerList = new ArrayList<String>();//create array of players;
+
+        // take in player name and add to the list
+        while (playerNum <= numOfPlayers) {  // Name each player 
+            System.out.print("Name Player " + playerNum +": ");
+            String playerName = scanner.nextLine();
+            playerList.add(playerName);
+            playerNum++;
+        }
+
+        Players players = new Players(playerList);
+        
+        for(int i=0; i<playerList.size(); i++) {
+            
+        }
         /*
         Game game = new Game(new USLeagueScorer());
         game.roll(5);
---
+++ src/Players.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+
+public class Players {
+    
+    // Data
+    private ArrayList<String> playerList = new  ArrayList<String>();
+    private ArrayList<Integer> scoreList = new ArrayList<Integer>();
+
+   
+    // Fields
+    
+    // Contructors
+    public Players(ArrayList<String> playerList) {
+        this.playerList = playerList;
+    }
+
+}
+
+
+    
+    
